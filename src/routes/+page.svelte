5ab1812dac4c2c04f2a73ac502b5fb18f5346746
Dennis Sonntag <sonntagdennis8@gmail.com>
2024-02-29 09:35:27 -0700

    setup tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="bg-slate-500 w-screen h-screen">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
